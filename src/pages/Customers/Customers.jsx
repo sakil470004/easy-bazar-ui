a31b0457f32272cda5a9c2a24fd36a007540e5d8
Mynul Islam <mynul.sakil@gmail.com>
2024-06-05 21:09:34 +0600

    refactor: Update Customers and Orders pages
    The commit refactors the Customers and Orders pages. It updates the page content to display an "Order list" heading and a table with order details. This improves the user experience by providing a clear and organized view of the orders.

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Customers() {
   return (
-    <div>Customers</div>
+    <div className="my-6 ">
+    <h2 className="text-2xl font-bold text-orange-400 uppercase">
+      Customer list
+    </h2>
+    
+
+    
+    </div>
   )
 }
 
