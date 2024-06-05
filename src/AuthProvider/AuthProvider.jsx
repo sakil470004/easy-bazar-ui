a337ada5274e71d849e0bb1ff44b1a42ad4a85a1
Mynul Islam <mynul.sakil@gmail.com>
2024-06-05 17:27:59 +0600

    remove old cart components

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -9,7 +9,6 @@ import {
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
-import toast from "react-hot-toast";
 
 export const AuthContext = createContext(null);
 
@@ -19,43 +18,14 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [cart, setCart] = useState([]);
+  
   const googleProvider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
-  const addToCart = (product) => {
-    //check if the product is already in the cart
-    if (user?.email) {
-      const exist = cart.find((p) => p._id === product._id);
-      const filteredCard = cart.filter((p) => p._id !== product._id);
-      let newCart = [];
-      let newObj={}
-      if (!exist) {
-        newObj = { ...product, quantity: 1, userEmail: user.email };
-        newCart = [...cart, newObj];
-      } else {
-        newObj = { ...exist, quantity: exist.quantity + 1 };
-        newCart = [...filteredCard,newObj];
-      }
-      fetch(`http://localhost:5000/cart`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newObj),
-      }).then((res) => res.json())
-      .then((data) => console.log(data));
-
-
-      setCart(newCart);
-      toast.success("Product added to cart");
-    } else {
-      toast.error("Please login to add product to cart");
-    }
-  };
+ 
   
   const signIn = (email, password) => {
     setLoading(true);
@@ -84,9 +54,7 @@ const AuthProvider = ({ children }) => {
       return unscubcribe();
     };
   }, []);
-  // useEffect(() => {
-  //   console.log(cart);
-  // }, [cart]);
+ 
   const authInfo = {
     user,
     googleLogin,
@@ -94,9 +62,7 @@ const AuthProvider = ({ children }) => {
     signIn,
     logout,
     loading,
-    cart,
-    setCart,
-    addToCart,
+    
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
